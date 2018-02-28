4cb94d915d02cabadb999f6d51a4d014231793f0
paktongko <paktongko90@gmail.com>
2018-02-28 17:52:35 +0800

    jquery practice

---
+++ jquery/documentready.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document Ready Event</title>
+</head>
+<body>
+	<div id="message" style="display: none;">Welcome to My Website</div>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/init.js"></script>
+</body>
+</html>
---
+++ jquery/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jquery example</title>
+</head>
+<body>
+	<p onclick="alert('works');">This is testing for jquery</p>
+	<p onclick="$(this).hide();">This is testing for jquery 2</p>
+	<p id="paragraph">jQuery external file</p>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/hide.js"></script>
+
+</body>
+</html>
