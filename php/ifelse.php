72621d961b38f801401d852bb13c0d59eb1c93b7
paktongko <paktongko90@gmail.com>
2018-03-01 17:42:41 +0800

    if/else tutorial

---
+++ php/ifelse.php
@@ -0,0 +1,12 @@
+<?php 
+	$name = 'Edah';
+
+	if($name == 'Faizul'){
+		echo 'correct name = '.$name;
+	}else if($name == 'Jamal'){
+		echo 'Name is = '. $name;
+	}else{
+		echo 'wrong name';
+	}
+
+ ?>
