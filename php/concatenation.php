53225ce7471a0328416ff831909f62dafa906c25
paktongko <paktongko90@gmail.com>
2018-03-01 17:30:39 +0800

    php tutorial

---
+++ php/concatenation.php
@@ -0,0 +1,9 @@
+<?php 
+	$day = 'Thursday';
+	$month = 'March';
+	$date = 01;
+	$year = 2018;
+
+	echo 'Today is '.$day.'. And the date is '.$month.' '.$date.' '.$year; // use single mark
+	echo "<br>Today is $day. And the date is $month $date $year"; //use double mark
+ ?>
